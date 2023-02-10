f79ccb8257cc8f28c70d3298a08490656e355530
Mandillah123* <official1337h4x0r@gmail.com>
2023-02-10 19:51:56 +0530

    working client server

---
+++ chat/client/app.js
@@ -0,0 +1,20 @@
+const socket=io("http://localhost:9000")
+
+const send=()=>{
+    const msg=document.getElementById('m').value;
+    socket.emit("chat message",msg)
+}
+
+socket.on("chat message",(msg)=>{
+    const para=document.createElement('p');
+    para.innerText=msg;
+    document.getElementById("messages").appendChild(para);
+
+})
+
+socket.on("new user notification",()=>{
+    const para=document.createElement('p')
+    para.innerHTML=`New User ${socket.id} connected`
+    document.getElementById("messages").appendChild(para)
+})
+
---
+++ chat/server/index.js
@@ -0,0 +1,26 @@
+const express=require("express")
+const {createServer}=require('http')
+const {Server}=require("socket.io")
+
+const app=express()
+const httpServer=createServer(app)
+const io=new Server(httpServer,{
+    cors:{
+        origin:["http://localhost:5500","http://127.0.0.1:5500"]
+    }  
+})
+
+io.on('connection',(socket)=>{
+    console.log("New Client Connected !")
+    socket.broadcast.emit("new user notification","hm")
+    socket.on("disconnect",(socket)=>{
+        console.log("Disconnected")
+    })
+    socket.on("chat message",(msg)=>{
+        io.emit("chat message",socket.id+": "+msg)
+    })
+});
+
+httpServer.listen(9000,()=>{
+    console.log("listening at port 9000")
+})
